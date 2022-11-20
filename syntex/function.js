c23bc0dae3b57e440475d71cd3d65f35f012ab10
Seounh ho Jeon <pcjoker10@gmail.com>
2022-11-20 22:13:54 +0900

    nodejs 강의 실습 - 함수로 정리

---
+++ syntex/function.js
@@ -0,0 +1,28 @@
+console.log('A');
+console.log('2');
+console.log('3');
+console.log('4');
+console.log('A');
+console.log('A');
+console.log('A');
+console.log('A');
+console.log('A');
+console.log('A');
+console.log('B');
+console.log('V');
+console.log('V');
+console.log('C');
+f123();
+f123();
+console.log(4);
+f123();
+
+function f123(){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+}
+
+
+
+
